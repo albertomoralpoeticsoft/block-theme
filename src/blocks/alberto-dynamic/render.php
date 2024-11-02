8e5d3fcbe018782571f284218faabab9fd7bd8dc
albertomoral <alberto.moral@poeticsoft.com>
2024-11-02 01:50:31 +0100

    Init Repo

---
+++ src/blocks/alberto-dynamic/render.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+?>
+<p <?php echo get_block_wrapper_attributes(); ?>>
+	<?php 
+    esc_html_e(
+      'Eh! que passssa',
+      'alberto-dynamic'
+    ); 
+  ?>
+</p>
