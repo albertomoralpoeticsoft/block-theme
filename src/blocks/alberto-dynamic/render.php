2b198358eda0882990b4c990570ff7ad317bc72a
albertomoral <alberto.moral@poeticsoft.com>
2024-11-04 15:04:13 +0100

    Sync Alberto

--- src/blocks/alberto-dynamic/render.php
+++ src/blocks/alberto-dynamic/render.php
@@ -3,11 +3,10 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
+<div <?php echo get_block_wrapper_attributes(); ?>>
 	<?php 
     esc_html_e(
-      'Eh! que passssa',
-      'alberto-dynamic'
+      'Dynamic block'
     ); 
   ?>
-</p>
+</div>
